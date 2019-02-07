97ddf6cc36683977baf2439ba400161cb055e683
Troels Henriksen <athas@sigkill.dk>
2019-02-07 08:42:33 +0100

    This does not belong here.

--- lib/github.com/diku-dk/lys/common.mk
+++ lib/github.com/diku-dk/lys/common.mk
@@ -16,9 +16,6 @@ LDFLAGS?=$(OPENCLFLAGS) -lm -lSDL2 -lSDL2_ttf
 $(PROGNAME): $(PROGNAME)_wrapper.o lib/github.com/diku-dk/lys/liblys.c
 	gcc lib/github.com/diku-dk/lys/liblys.c -I. -DPROGHEADER='"$(PROGNAME)_wrapper.h"' $(PROGNAME)_wrapper.o -o $@ $(LDFLAGS)
 
-lib: futhark.pkg
-	futhark pkg sync
-
 # We do not want warnings and such for the generated code.
 $(PROGNAME)_wrapper.o: $(PROGNAME)_wrapper.c
 	gcc -o $@ -c $< $(NOWARN_CFLAGS)
